cff7f42869bbd5a8dcf7d4061469a2c39ad0c994
Fakhri <fakhialtafian010@gmail.com>
2025-11-30 11:37:47 +0700

    bug fix menu-icon di utama & login

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 // aktif navbar, shoppingcart
 const navbarNav = document.querySelector('.navbar-nav');
 const shoppingcart = document.querySelector('.shopping-cart');
+const menu = document.querySelector('#menu-icon ');
+const cart = document.querySelector('#cart-icon');
 
 // menu di klik
-document.querySelector('#cart-icon').onclick = () => {
-    shoppingcart.classList.toggle('active');
+if (menu) {
+    menu.onclick = () => {
+        navbarNav.classList.toggle('active');
+        if(shoppingcart)  shoppingcart.classList.remove('active');
+    }
 }
-document.querySelector('#menu-icon').onclick = () => {
-    navbarNav.classList.toggle('active');
+
+if (cart) {
+    cart.onclick = () => {
+        shoppingcart.classList.toggle('active');
+        navbarNav.classList.remove('active');
+    }
 }
 
 // klik di luar sidebar untuk menghilangkan navbarnya
-const menu = document.querySelector('#menu-icon ');
-const cart = document.querySelector('#cart-icon');
-const trash = document.querySelector(".add-cart")
-
 document.addEventListener('click', function(e) {
     if(!menu.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
